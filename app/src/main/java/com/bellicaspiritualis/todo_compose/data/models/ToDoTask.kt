05586db137b94c66442b381dee5d0ce8607df454
silence <ggrobelny@outlook.fr>
2021-10-25 20:57:57 +0200

    first commit

---
+++ app/src/main/java/com/bellicaspiritualis/todo_compose/data/models/ToDoTask.kt
@@ -0,0 +1,14 @@
+package com.bellicaspiritualis.todo_compose.data.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.bellicaspiritualis.todo_compose.util.Constants.DATABASE_TABLE
+
+@Entity(tableName = DATABASE_TABLE)
+data class ToDoTask (
+    @PrimaryKey
+    val id: Int = 0,
+    val title: String,
+    val description: String,
+    val priority: Priority
+)
