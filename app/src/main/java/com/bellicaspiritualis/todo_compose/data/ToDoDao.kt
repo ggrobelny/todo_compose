d995fc5a860c6d4a07387823c77a16cc1ebed4de
ggrobelny <ggrobelny@outlook.fr>
2021-10-26 19:31:45 +0200

    ToDoDao another query's

--- app/src/main/java/com/bellicaspiritualis/todo_compose/data/ToDoDao.kt
+++ app/src/main/java/com/bellicaspiritualis/todo_compose/data/ToDoDao.kt
@@ -1,9 +1,6 @@
 package com.bellicaspiritualis.todo_compose.data
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import kotlinx.coroutines.flow.Flow
 import com.bellicaspiritualis.todo_compose.data.models.ToDoTask
 
@@ -18,4 +15,22 @@ interface ToDoDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun addTask(toDoTask: ToDoTask)
+
+    @Update
+    suspend fun updateTask(toDoTask: ToDoTask)
+
+    @Delete
+    suspend fun deleteTask(toDoTask: ToDoTask)
+
+    @Query("DELETE FROM todo_table")
+    suspend fun deleteAllTasks()
+
+    @Query("SELECT * FROM todo_table WHERE title LIKE :searchQuery OR description LIKE :searchQuery")
+    fun searchDatabase(searchQuery: String): Flow<List<ToDoTask>>
+
+    @Query("SELECT * FROM todo_table ORDER BY CASE WHEN priority LIKE 'L%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'H%' THEN 3 END")
+    fun sortByLowPriority(): Flow<List<ToDoTask>>
+
+    @Query("SELECT * FROM todo_table ORDER BY CASE WHEN priority LIKE 'H%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'L%' THEN 3 END")
+    fun sortByHighPriority(): Flow<List<ToDoTask>>
 }
