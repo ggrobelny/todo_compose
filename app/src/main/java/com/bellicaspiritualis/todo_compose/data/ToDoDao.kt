ef5702328374d78a530f6c7df348c67f1905b0e5
ggrobelny <ggrobelny@outlook.fr>
2021-10-26 19:08:35 +0200

    tododao interface: query task by id and query insert with suspend fun

--- app/src/main/java/com/bellicaspiritualis/todo_compose/data/ToDoDao.kt
+++ app/src/main/java/com/bellicaspiritualis/todo_compose/data/ToDoDao.kt
@@ -1,7 +1,20 @@
 package com.bellicaspiritualis.todo_compose.data
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+import com.bellicaspiritualis.todo_compose.data.models.ToDoTask
 
 @Dao
 interface ToDoDao {
+
+    @Query("SELECT * FROM todo_table ORDER BY id ASC")
+    fun getAllTasks(): Flow<List<ToDoTask>>
+
+    @Query("SELECT * FROM todo_table WHERE id=:taskId")
+    fun getSelectedTask(taskId: Int): Flow<ToDoTask>
+
+    @Insert
+    suspend fun addTask(toDoTask: ToDoTask)
 }
