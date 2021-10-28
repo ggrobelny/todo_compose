fb7fbfe63a571a2c50e90ef2c82a57961bac3b95
ggrobelny <ggrobelny@outlook.fr>
2021-10-28 15:23:25 +0200

    onSearchClicked

--- app/src/main/java/com/bellicaspiritualis/todo_compose/ui/screens/list/ListAppBar.kt
+++ app/src/main/java/com/bellicaspiritualis/todo_compose/ui/screens/list/ListAppBar.kt
@@ -1,20 +1,27 @@
 package com.bellicaspiritualis.todo_compose.ui.screens.list
 
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.bellicaspiritualis.todo_compose.ui.theme.topAppBarBackgroundColor
 import com.bellicaspiritualis.todo_compose.ui.theme.topAppBarContentColor
+import com.bellicaspiritualis.todo_compose.R
+import com.bellicaspiritualis.todo_compose.data.models.Priority
 
 @Composable
 fun ListAppBar() {
-    DefaultListAppBar()
+    DefaultListAppBar(
+        onSearchClicked = {}
+    )
 }
 
 @Composable
-fun DefaultListAppBar() {
+fun DefaultListAppBar(
+    onSearchClicked: () -> Unit
+) {
     TopAppBar(
         title = {
             Text(
@@ -22,12 +29,45 @@ fun DefaultListAppBar() {
                 color = MaterialTheme.colors.topAppBarContentColor
                 )
         },
+        actions = {
+            ListAppBarActions(onSearchClicked = onSearchClicked)
+        },
+
         backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor
     )
 }
 
 @Composable
+fun ListAppBarActions(
+    onSearchClicked: () -> Unit
+) {
+    SearchAction(onSearchClicked = onSearchClicked)
+}
+
+@Composable
+fun SearchAction(
+    onSearchClicked: () -> Unit
+) {
+    IconButton(onClick = { onSearchClicked() }
+    ) {
+        Icon(imageVector = Icons.Filled.Search,
+            contentDescription = stringResource(id = R.string.search_action),
+            tint = MaterialTheme.colors.topAppBarContentColor
+        )
+    }
+}
+
+@Composable
+fun SortAction(
+    onSortClicked: (Priority) -> Unit
+) {
+
+}
+
+@Composable
 @Preview
 fun DefaultListAppBarPreview() {
-    DefaultListAppBar()
+    DefaultListAppBar(
+        onSearchClicked = {}
+    )
 }
