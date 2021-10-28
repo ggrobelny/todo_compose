9bd77d2ade626a5ea3930717b9973b098b987773
ggrobelny <ggrobelny@outlook.fr>
2021-10-28 17:03:09 +0200

    expanded DropDownItem icon filter etc

--- app/src/main/java/com/bellicaspiritualis/todo_compose/ui/screens/list/ListAppBar.kt
+++ app/src/main/java/com/bellicaspiritualis/todo_compose/ui/screens/list/ListAppBar.kt
@@ -4,23 +4,31 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.bellicaspiritualis.todo_compose.ui.theme.topAppBarBackgroundColor
 import com.bellicaspiritualis.todo_compose.ui.theme.topAppBarContentColor
 import com.bellicaspiritualis.todo_compose.R
 import com.bellicaspiritualis.todo_compose.data.models.Priority
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import com.bellicaspiritualis.todo_compose.components.PriorityItem
 
 @Composable
 fun ListAppBar() {
     DefaultListAppBar(
-        onSearchClicked = {}
+        onSearchClicked = {},
+        onSortClicked = {}
     )
 }
 
 @Composable
 fun DefaultListAppBar(
-    onSearchClicked: () -> Unit
+    onSearchClicked: () -> Unit,
+    onSortClicked: (Priority) -> Unit
 ) {
     TopAppBar(
         title = {
@@ -30,7 +38,10 @@ fun DefaultListAppBar(
                 )
         },
         actions = {
-            ListAppBarActions(onSearchClicked = onSearchClicked)
+            ListAppBarActions(
+                onSearchClicked = onSearchClicked,
+                onSortClicked = onSortClicked
+                )
         },
 
         backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor
@@ -39,9 +50,11 @@ fun DefaultListAppBar(
 
 @Composable
 fun ListAppBarActions(
-    onSearchClicked: () -> Unit
+    onSearchClicked: () -> Unit,
+    onSortClicked: (Priority) -> Unit
 ) {
     SearchAction(onSearchClicked = onSearchClicked)
+    SortAction(onSortClicked = onSortClicked)
 }
 
 @Composable
@@ -61,13 +74,50 @@ fun SearchAction(
 fun SortAction(
     onSortClicked: (Priority) -> Unit
 ) {
+    var expanded by remember { mutableStateOf(false)}
+    IconButton(onClick = { expanded = true }) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_filter_list),
+            contentDescription = stringResource(id = R.string.sort_action)
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }) {
+            DropdownMenuItem(
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.LOW)
+                }
+            ) {
+                PriorityItem(priority = Priority.LOW)
+            }
 
+            DropdownMenuItem(
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.HIGH)
+                }
+            ) {
+                PriorityItem(priority = Priority.HIGH)
+            }
+
+            DropdownMenuItem(
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.NONE)
+                }
+            ) {
+                PriorityItem(priority = Priority.NONE)
+            }
+        }
+    }
 }
 
 @Composable
 @Preview
 fun DefaultListAppBarPreview() {
     DefaultListAppBar(
-        onSearchClicked = {}
+        onSearchClicked = {},
+        onSortClicked = {}
     )
 }
