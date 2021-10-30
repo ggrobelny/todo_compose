a999a61ca9a242444a86c1e714a07d52955921c1
ggrobelny <ggrobelny@outlook.fr>
2021-10-30 12:35:11 +0200

    new color, new dimension for Surface -> Column(empty)

--- app/src/main/java/com/bellicaspiritualis/todo_compose/ui/screens/list/ListContent.kt
+++ app/src/main/java/com/bellicaspiritualis/todo_compose/ui/screens/list/ListContent.kt
@@ -1,19 +1,39 @@
 package com.bellicaspiritualis.todo_compose.ui.screens.list
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import com.bellicaspiritualis.todo_compose.data.models.ToDoTask
+import com.bellicaspiritualis.todo_compose.ui.theme.TASK_ITEM_ELEVATION
+import com.bellicaspiritualis.todo_compose.ui.theme.taskItemBackgroundColor
 
 @Composable
 fun ListContent() {
 }
 
+@ExperimentalMaterialApi
 @Composable
 fun TaskItem(
     todoTask: ToDoTask,
     navigateToTaskScreen: (taskId: Int) -> Unit
 ) {
-    Surface() {
-
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth(),
+        color = MaterialTheme.colors.taskItemBackgroundColor,
+        shape = RectangleShape,
+        elevation = TASK_ITEM_ELEVATION,
+        onClick = {
+            navigateToTaskScreen(todoTask.id)
+        }
+    ) {
+        Column() {
+            
+        }
     }
 }
--- app/src/main/java/com/bellicaspiritualis/todo_compose/ui/theme/Color.kt
+++ app/src/main/java/com/bellicaspiritualis/todo_compose/ui/theme/Color.kt
@@ -20,10 +20,13 @@ val MediumPriorityColor = Color(0xFFFFC114)
 val HighPriorityColor = Color(0xFFFF4646)
 val NonePriorityColor = Color(0xFF7C7C7C)
 
+val Colors.taskItemBackgroundColor: Color
+    @Composable
+    get() = if(isLight) Color.White else DarkGray
+
 val Colors.fabBackgroundColor: Color
     @Composable
     get() = if(isLight) Zloty else Zloty
-
 
 val Colors.topAppBarContentColor: Color
     @Composable
