5f53948e9677745a2e52a9ea9f0cca8fc3276a92
ggrobelny <ggrobelny@outlook.fr>
2021-10-30 12:24:51 +0200

    ListContent.kt @Composable fun TaskItem and surface(empty)

--- app/src/main/java/com/bellicaspiritualis/todo_compose/ui/screens/list/ListContent.kt
+++ app/src/main/java/com/bellicaspiritualis/todo_compose/ui/screens/list/ListContent.kt
@@ -1,7 +1,19 @@
 package com.bellicaspiritualis.todo_compose.ui.screens.list
 
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import com.bellicaspiritualis.todo_compose.data.models.ToDoTask
 
 @Composable
 fun ListContent() {
+}
+
+@Composable
+fun TaskItem(
+    todoTask: ToDoTask,
+    navigateToTaskScreen: (taskId: Int) -> Unit
+) {
+    Surface() {
+
+    }
 }
