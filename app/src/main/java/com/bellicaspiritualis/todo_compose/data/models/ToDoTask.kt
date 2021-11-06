a755dc45b0909cb8d37aae3bc331d9718bca6a7b
ggrobelny <ggrobelny@outlook.fr>
2021-11-06 21:42:21 +0100

    bug fixed with @PrimaryKey(autoGenerate = true) hahaha, undo deleted functions added

--- app/src/main/java/com/bellicaspiritualis/todo_compose/data/models/ToDoTask.kt
+++ app/src/main/java/com/bellicaspiritualis/todo_compose/data/models/ToDoTask.kt
@@ -6,7 +6,7 @@ import com.bellicaspiritualis.todo_compose.util.Constants.DATABASE_TABLE
 
 @Entity(tableName = DATABASE_TABLE)
 data class ToDoTask (
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val title: String,
     val description: String,
