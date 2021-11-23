8be6085fb9ac448d5fee6cfb692c78e1dace07cc
ggrobelny <ggrobelny@outlook.fr>
2021-11-23 22:02:13 +0100

    dependency update & cleanup + config change -> snackbar, searchappbar bugs fixed et c

--- app/src/main/java/com/bellicaspiritualis/todo_compose/navigation/destinations/TaskComposable.kt
+++ app/src/main/java/com/bellicaspiritualis/todo_compose/navigation/destinations/TaskComposable.kt
@@ -6,7 +6,7 @@ import androidx.compose.runtime.getValue
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.navArgument
+import androidx.navigation.navArgument
 import com.bellicaspiritualis.todo_compose.ui.screens.task.TaskScreen
 import com.bellicaspiritualis.todo_compose.ui.viewmodels.SharedViewModel
 import com.bellicaspiritualis.todo_compose.util.Action
--- app/src/main/java/com/bellicaspiritualis/todo_compose/util/TrailingIconState.kt
+++
@@ -1,6 +0,0 @@
-package com.bellicaspiritualis.todo_compose.util
-
-enum class TrailingIconState {
-    READY_TO_DELETE,
-    READY_TO_CLOSE
-}
